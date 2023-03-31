1fd6bee1400bee905e8845c070e6610b64541afb
Jon Kuperman <jon.kuperman@gmail.com>
2023-03-31 16:41:11 +0100

    take 1 at a state machine

--- src/simple-markdown-parser.ts
+++ src/simple-markdown-parser.ts
@@ -1,56 +1,167 @@
+enum State {
+  TEXT,
+  HEADER,
+  LIST_ITEM,
+  BOLD,
+  ITALIC,
+  LINK,
+  LINK_TEXT,
+  LINK_HREF,
+}
+
 export default class SimpleMarkdownParser {
-  parse(markdown: string): string {
-    return this.parseLines(markdown.split("\n")).join("");
+  private output: string;
+
+  constructor() {
+    this.output = "";
   }
 
-  parseLines(lines: string[]): string[] {
-    let html: string[] = [];
+  parse(input: string): string {
+    this.output = "";
+    const lines = input.split("\n");
+    let state: State = State.TEXT;
+    let buffer = "";
+    let linkText = "";
+    let linkHref = "";
 
-    for (let i = 0; i < lines.length; i++) {
-      let line = lines[i].trim();
-
-      // Headers
-      const headerMatch = line.match(/^(#+)\s+(.*)$/);
-      if (headerMatch) {
-        const level = headerMatch[1].length;
-        html.push(`<h${level}>${this.parseInline(headerMatch[2])}</h${level}>`);
-        continue;
+    const flushBuffer = () => {
+      switch (state) {
+        case State.HEADER:
+          const level = buffer.length;
+          this.output += `<h${level}>`;
+          state = State.TEXT;
+          break;
+        case State.LIST_ITEM:
+          this.output += "<li>";
+          break;
+        case State.BOLD:
+          this.output += "<strong>";
+          break;
+        case State.ITALIC:
+          this.output += "<em>";
+          break;
+        case State.LINK_HREF:
+          this.output += `<a href="${buffer}">${linkText}</a>`;
+          linkText = "";
+          linkHref = "";
+          break;
       }
+      buffer = "";
+    };
 
-      // Lists
-      let listMatch = line.match(/^(-|\*|\+)\s+(.*)$/);
-      if (listMatch) {
-        const items: string[] = [];
-        while (listMatch && i < lines.length) {
-          items.push(`<li>${this.parseInline(listMatch[2])}</li>`);
-          i++;
-          if (i < lines.length) {
-            line = lines[i].trim();
-            listMatch = line.match(/^(-|\*|\+)\s+(.*)$/);
+    const closeTag = () => {
+      switch (state) {
+        case State.HEADER:
+          const level = buffer.length;
+          this.output += `</h${level}>`;
+          break;
+        case State.LIST_ITEM:
+          this.output += "</li>";
+          break;
+        case State.BOLD:
+          this.output += "</strong>";
+          break;
+        case State.ITALIC:
+          this.output += "</em>";
+          break;
+      }
+    };
+
+    for (const line of lines) {
+      let i = 0;
+      while (i < line.length) {
+        const char = line[i];
+
+        if (state === State.TEXT) {
+          if (char === "#" && (i === 0 || line[i - 1] === "\n")) {
+            flushBuffer();
+            state = State.HEADER;
+          } else if (char === "-" && (i === 0 || line[i - 1] === "\n")) {
+            flushBuffer();
+            state = State.LIST_ITEM;
+          } else if (char === "*" && line[i + 1] === "*") {
+            flushBuffer();
+            state = State.BOLD;
+            i++;
+          } else if (char === "*" && line[i + 1] !== "*") {
+            flushBuffer();
+            state = State.ITALIC;
+          } else if (char === "[") {
+            flushBuffer();
+            state = State.LINK_TEXT;
+          } else {
+            this.output += char;
+          }
+        } else if (state === State.HEADER) {
+          if (char === " ") {
+            flushBuffer();
+          } else {
+            buffer += char;
+          }
+        } else if (state === State.LIST_ITEM) {
+          if (char === " ") {
+            flushBuffer();
+          } else {
+            this.output += char;
+          }
+        } else if (state === State.BOLD) {
+          if (char === "*" && line[i + 1] === "*") {
+            closeTag();
+            state = State.TEXT;
+            i++;
+          } else {
+            this.output += char;
+          }
+        } else if (state === State.ITALIC) {
+          if (char === "*") {
+            closeTag();
+            state = State.TEXT;
+          } else {
+            this.output += char;
+          }
+        } else if (state === State.LINK_TEXT) {
+          if (char === "]") {
+            state = State.LINK_HREF;
+          } else {
+            linkText += char;
+          }
+        } else if (state === State.LINK_HREF) {
+          if (char === "(") {
+            // Do nothing, just skip the character
+          } else if (char === ")") {
+            flushBuffer();
+            state = State.TEXT;
+          } else {
+            buffer += char;
           }
         }
-        html.push(`<ul>${items.join("")}</ul>`);
-        i--;
-        continue;
+
+        i++;
       }
 
-      // Paragraphs
-      if (line.length > 0) {
-        html.push(`<p>${this.parseInline(line)}</p>`);
+      // Process end of line
+      if (state === State.HEADER || state === State.LIST_ITEM) {
+        closeTag();
+        this.output += "\n";
+        state = State.TEXT;
+      } else if (
+        state !== State.BOLD &&
+        state !== State.ITALIC &&
+        state !== State.LINK_HREF
+      ) {
+        this.output += "\n";
       }
     }
 
-    return html;
-  }
+    // Close any unclosed tags at the end of the input
+    if (
+      state === State.BOLD ||
+      state === State.ITALIC ||
+      state === State.LINK_HREF
+    ) {
+      closeTag();
+    }
 
-  parseInline(line: string): string {
-    // Bold and italic
-    line = line.replace(/(\*\*|__)(.*?)\1/g, "<strong>$2</strong>");
-    line = line.replace(/(\*|_)(.*?)\1/g, "<em>$2</em>");
-
-    // Links
-    line = line.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
-
-    return line;
+    return this.output.trim();
   }
 }
